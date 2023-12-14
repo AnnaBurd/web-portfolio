77ad4a4215930cdc4e2f9327caef582ba2a73929
Anna Burdanova <burdanovaas@gmail.com>
2023-12-14 19:48:36 +0700

    feat: :construction: style error page

--- src/app/[lang]/[...not-found]/page.tsx
+++ src/app/[lang]/[...not-found]/page.tsx
@@ -1,12 +1,34 @@
-import Link from "next/link";
+import { fontPrimary } from "@/app/fonts";
+// import Link from "next/link";
+
+import Link from "@/app/components/ui/Link";
 
 export default function NotFound() {
   return (
-    <div>
-      NOT FOUND PAGE WORKAROUND
-      <Link href="/" replace scroll={false}>
-        Return Home
-      </Link>
-    </div>
+    <section className="relative flex min-h-screen w-full">
+      <div className=" absolute inset-6 z-0 bg-[--background-accent]"></div>
+      <div className="wrapper  z-10 flex w-full items-center justify-stretch ">
+        <div className="absolute top-24 -mt-2 mb-1 text-lg font-semibold uppercase tracking-widest opacity-30">
+          404
+        </div>
+        <div className="mx-auto w-full max-w-prose text-center">
+          <h1
+            className={
+              fontPrimary.className +
+              `   mb-6 text-4xl font-black leading-tight [text-wrap:balance]`
+            }
+          >
+            Page not found :(
+          </h1>
+          <p className="my-auto mb-8 text-base leading-relaxed tracking-wide [text-wrap:balance]">
+            The page you are looking for doesn&apos;t exist or has been moved.
+          </p>
+          {/* <Link href="/" replace scroll={false}> */}
+          <Link href="/" replace={true} scroll={false}>
+            go home
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
--- src/app/components/ui/Link.tsx
+++ src/app/components/ui/Link.tsx
@@ -6,13 +6,23 @@ type Props = {
   href: string;
   children: React.ReactNode;
   className?: string;
+  replace?: boolean;
+  scroll?: boolean;
 };
 
-const Button: React.FC<Props> = ({ children, href, className }) => {
+const Button: React.FC<Props> = ({
+  children,
+  href,
+  className,
+  replace = false,
+  scroll = true,
+}) => {
   return (
     <Link
       className={`${styles["link-highlight"]}  text-base font-medium ${className}`}
       href={href}
+      replace={replace}
+      scroll={scroll}
     >
       {children}
     </Link>
