b0f2906ea2ba16f3c9fb9e7cd17b6edbac9486b0
Anna Burdanova <burdanovaas@gmail.com>
2024-01-10 18:05:00 +0700

    feat: :memo: update translation functionality on the project's pages

--- src/app/[lang]/work/[slug]/page.tsx
+++ src/app/[lang]/work/[slug]/page.tsx
@@ -6,13 +6,16 @@ import Markdown from "./components/Markdown";
 
 import { Locale } from "@/app/i18n/i18n-config";
 import { getProjectData } from "@/scripts/getProjectsData";
+import { useTranslation } from "@/app/i18n/server";
 
 type Props = {
   params: { slug: string; lang: Locale };
 };
 
-const ProjectPage: React.FC<Props> = ({ params }) => {
+const ProjectPage: React.FC<Props> = async ({ params }) => {
   const projectData = getProjectData(params.slug, params.lang);
+
+  const translation = (await useTranslation(params.lang, "project")).t;
 
   return (
     <article className=" flex min-h-screen w-full overflow-x-hidden">
@@ -21,7 +24,7 @@ const ProjectPage: React.FC<Props> = ({ params }) => {
       <div className="wrapper z-10  ">
         <div className=" mb-6 mt-44 flex flex-col items-center text-center md:mb-20 ">
           <h4 className="mb-1 text-sm font-semibold uppercase tracking-widest opacity-30">
-            PROJECT SHOWCASE
+            {translation("title")}
           </h4>
           <h2
             className={
@@ -40,14 +43,14 @@ const ProjectPage: React.FC<Props> = ({ params }) => {
               href={projectData.frontmatterData.repositoryUrl}
               className="mr-6"
             >
-              github repo
+              {translation("links.github")}
             </Link>
 
             <Link
               href={projectData.frontmatterData.previewUrl}
               className="relative"
             >
-              visit live site &nbsp;&nbsp;&nbsp;
+              {translation("links.live")} &nbsp;&nbsp;&nbsp;
               <span className="absolute -right-0.5 top-1/2 -translate-y-1/2">
                 <ArrowRight size={16} weight="regular" />
               </span>
